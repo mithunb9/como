61d4f5b03351e8843852ff548b153d4908451088
Balstun <ewizard512@gmail.com>
2023-06-07 17:22:37 -0500

    Sample test (Himansu) to drive COMO to target using Motion capture data

---
+++ workspace/src/centralized_server/src/oval_track_test.py
@@ -0,0 +1,60 @@
+#!/usr/bin/evn python
+
+import roslib
+import rospy
+from rospy import init_node, Subscriber, Publisher, get_param
+from rospy import Rate, is_shutdown, ROSInterrupterException, spin, on_shutdown
+from barc.msg import ECU, Encoder
+from tf.transformations import euler_from_quaternion
+import numpy as np
+from math import atan2
+from geometry_msgs.msg import Point, Twist, PoseStamped
+from std_msgs.msg import Float64
+from numpy import pi
+
+
+def main():
+    rospy.init_node("speed_controller")
+    nh = Publisher('ecu_pwm', ECU, queue_size=10)
+    rateHz = 50
+    dt = 1.0/rateHz
+    rate = Rate(rateHz)
+    while not rospy.is_shutdown():
+        msg = rospy.wait_for_message("/vrpn_client_node/como/pose", PoseStamped)
+        # msg2
+        
+        x = msg.pose.position.x
+        y = msg.pose.position.y
+        #goalX
+        #goalY
+        rot_q = msg.pose.orientation
+        (roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q,w])
+        inc_x = goalX - x
+        inc_y = goalY - y
+        print(theta)
+        theta = -theta
+        angle_to_goal = atan2(inc_x, inc_y)
+        distance_to_goal = np.sqrt(np.square(inc_x) + np.square(inc_y))
+        servo_to_goal = (angle_to_goal - theta)
+        throttle = 1558 + (distance_to_goal -0.2) * 18
+        if throttle > 1584:
+            throttle = 1584
+        steering = 1492 + 335 * (angle_to_goal - theta)
+        if steering > 1984:
+            steering = 1984
+        elif steering < 984:
+            steering = 984
+        if steering >= 1984 and distance_to_goal > 0.5:
+            throttle = 1594
+        if steering <= 984 and distance_to-_goal > 0.5:
+            throttle = 1594
+        ecu_cmd = ECU(throttle, steering)
+        nh.publish(ecu_cmd)
+        rate.sleep()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptionException:
+        rospy.logfatal("ROS Interrupt. Shutting down speed_controller node")
+        pass
