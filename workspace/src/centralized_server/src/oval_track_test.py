96cc3dc58f3709f258a88d9816a5b06c3f9a8b61
Balstun <ewizard512@gmail.com>
2023-06-15 13:53:48 -0500

    Temporary commit for virtual_track

--- workspace/src/centralized_server/src/oval_track_test.py
+++ workspace/src/centralized_server/src/oval_track_test.py
@@ -12,25 +12,53 @@ from geometry_msgs.msg import Point, Twist, PoseStamped
 from std_msgs.msg import Float64
 from numpy import pi
 
+from como_tracks/oval_track_sim import *
 
 def main():
     rospy.init_node("speed_controller")
     nh = Publisher('ecu_pwm', ECU, queue_size=10)
-    rateHz = 50
+    rateHz = 100 # 120 Hz is preferred, but 100 Hz is configured in launch file (vrpn_client_ros)
     dt = 1.0/rateHz
     rate = Rate(rateHz)
+
+    file = os.path.join('como_tracks', 'tracks', 'oval_track_single_centerline3.txt.')
+    x, y = load_tack_txt(file)
+    x0, y0 = x[0], y[0]
+    x1, y1 = x[1], y[1]
+    dyn = 'singleTrack'
+
+    v = 1
+    kp = 1
+    theta0 = np.arctan(y1 - y0, x1 - x0)
+    lookahead = 0.5
+    dt = 0.05
+    steer_min, steer_max = -np.pi / 5, np.pi / 5
+
+    robot = SingleTrack(x=x0, y=y0, heading=theta0, v=v, lr=0.1, lf=0.1, dt=dt)
+
+    x_track, y_track = [], []
+
     while not rospy.is_shutdown():
+        w1 = 0
+        w2 = 0
         msg = rospy.wait_for_message("/vrpn_client_node/COMO4/pose", PoseStamped)
-        msg2 = rospy.wait_for_message("/vrpn_client_node/target_drone/pose", PoseStamped)
-        
-        x = msg.pose.position.x
-        y = msg.pose.position.y
-        goalX = msg2.pose.position.x
-        goalY = msg2.pose.position.y
+        robot_pos = np.array([msg.pose.position.x + w1, msg.pose.position.y + w2])
+        waypoints_ahead_x, waypoints_ahead_y, dist = get_waypoints_ahead_looped(x, y, robot_pos, lookahead)
+        wp = np.arry([waypoints_ahead_x[-1], waypoints_ahead_y[-1]])
+
+        accel = 0
+        steer_angle = kp * get_arc_curvature(wp, dist, robot_pos, robot.heading)
+        steer_angle = np.clip(steer_angle, steer_min, steer_max)
+        u = [accel, steer_angle]
+        robot.update(u)
+
+        goal_x = wp[0]
+        goal_y = wp[1]
+
         rot_q = msg.pose.orientation
         (roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q,w])
-        inc_x = goalX - x
-        inc_y = goalY - y
+        inc_x = goal_x - x
+        inc_y = goal_y - y
         print(theta)
         theta = -theta
         angle_to_goal = atan2(inc_x, inc_y)
@@ -55,6 +83,6 @@ def main():
 if __name__ == "__main__":
     try:
         main()
-    except rospy.ROSInterruptException:
+    except rospy.ROSInterrupException:
         rospy.logfatal("ROS Interrupt. Shutting down speed_controller node")
         pass
