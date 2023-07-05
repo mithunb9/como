b9916ccb7653c62842d43a1cd81464aa3d25f050
Balstun <ewizard512@gmail.com>
2023-07-05 11:39:10 -0500

    Update virtual track with local changes
    Add data folder to hold logs. Older logs included.
    
    Update oval_track_test.py to automatically create logs
    
    Use the /ecu topic to control the COMO
    
    Update the como_tracks package with the Python 2.7 implementation

--- workspace/src/centralized_server/setup.py
+++ workspace/src/centralized_server/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
         packages=['virtual_track', 'virtual_track.como_tracks'],
-        package_dir={'': 'src'}
+        package_dir={'': 'scripts'}
 )
 
 setup(**setup_args)
--- workspace/src/centralized_server/src/virtual_track/oval_track_test.py
+++ workspace/src/centralized_server/src/virtual_track/oval_track_test.py
@@ -10,8 +10,9 @@ from tf.transformations import euler_from_quaternion
 import numpy as np
 from math import atan2
 from geometry_msgs.msg import Point, Twist, PoseStamped
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, String
 from numpy import pi
+from datetime import datetime
 
 from como_tracks.oval_track_sim import * 
 
@@ -22,16 +23,33 @@ def bound_servo_angle(servo_angle):
         servo_angle += np.pi * 2
     return servo_angle
 
+x_track, y_track, heading_track, waypoint_x_track, waypoint_y_track = [], [], [], [], []
+
+rospack = rospkg.RosPack()
+package_path = rospack.get_path('centralized_server')
+
+def log_track_data(x_track, y_track):
+    timestamp = datetime.now()
+    formatted_timestamp = timestamp.strftime("%Y-%m-%d_%H:%M:%S")
+    fileName = "oval_track_single_lane_pos" + formatted_timestamp + ".txt"
+
+    path = os.path.join(package_path, 'data', 'logs', fileName)
+    f = open(path, 'w')
+    f.write("x,y,heading,waypoint_x,waypoint_y\n")
+    for i in range(len(x_track)):
+        row = str(x_track[i]) + ',' + str(y_track[i]) + ',' + str(heading_track[i]) + ',' + str(waypoint_x_track[i]) + ',' + str(waypoint_y_track[i]) + '\n'
+        f.write(row)
+    f.close()
+
 def main():
     rospy.init_node("speed_controller")
-    nh = Publisher('ecu_pwm', ECU, queue_size=10)
+    #nh = Publisher('ecu_pwm', ECU, queue_size=10)
+    nh = Publisher('ecu', ECU, queue_size=10)
     rateHz = 100 # 120 Hz is preferred, but 100 Hz is configured in launch file (vrpn_client_ros)
     dt = 1.0/rateHz
     rate = Rate(rateHz)
 
-    rospack = rospkg.RosPack()
-    package_path = rospack.get_path('test_launches')
-    file = os.path.join(package_path, 'scripts', 'virtual_track', 'como_tracks', 'tracks', 'oval_track_single_centerline4.txt')
+    file = os.path.join(package_path, 'src', 'virtual_track', 'como_tracks', 'tracks', 'oval_track_single_centerline4.txt')
     print(file)
     x, y = load_tack_txt(file)
     x0, y0 = x[0], y[0]
@@ -41,13 +59,11 @@ def main():
     v = 1
     kp = 1
     theta0 = np.arctan2(y1 - y0, x1 - x0)
-    lookahead = 0.3
+    lookahead = 0.25
     dt = 0.05
     steer_min, steer_max = -np.pi / 4, np.pi / 4
 
     robot = SingleTrack(x=x0, y=y0, heading=theta0, v=v, lr=0.1, lf=0.1, dt=dt)
-
-    x_track, y_track = [], []
 
     while not rospy.is_shutdown():
         msg = rospy.wait_for_message("/vrpn_client_node/COMO4/pose", PoseStamped)
@@ -60,6 +76,9 @@ def main():
 
         goal_x = wp[0]
         goal_y = wp[1]
+        waypoint_x_track.append(goal_x)
+        waypoint_y_track.append(goal_y)
+        
         print("Pos", robot_pos[0], robot_pos[1])
         rot_q = msg.pose.orientation
         (roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
@@ -67,30 +86,32 @@ def main():
         inc_y = goal_y - robot_pos[1]
         print("Way", goal_x, goal_y)
         print("Dis", inc_x, inc_y)
-        #theta = -theta
+        heading_track.append(theta)
+
         angle_to_goal = atan2(inc_y, inc_x) - np.pi/2
         print("Ang", theta, angle_to_goal)
         distance_to_goal = np.sqrt(np.square(inc_x) + np.square(inc_y))
-        servo_to_goal = (angle_to_goal - theta)
-        servo_to_goal = -servo_to_goal
+        servo_to_goal = (theta - angle_to_goal)
         if abs(servo_to_goal) > np.pi:
             servo_to_goal = bound_servo_angle(servo_to_goal)
         print(servo_to_goal)
-        #throttle = 1558 + (distance_to_goal) * 50
-        throttle = 1585
-        #if throttle > 1584:
-        #    throttle = 1584
+        
+        #throttle = 1630
+        motor = 6.5 
+        
+        '''
         steering = 1524 + 250 * servo_to_goal
         if steering > 1824:
             steering = 1824
         elif steering < 1224:
             steering = 1224
         print(throttle, steering)
-        #if steering >= 1984 and distance_to_goal > 0.5:
-        #    throttle = 1894
-        #if steering <= 984 and distance_to_goal > 0.5:
-        #    throttle = 1894
-        ecu_cmd = ECU(throttle, steering)
+        '''
+        servo_to_goal = np.clip(servo_to_goal, steer_min, steer_max)
+        servo_to_goal += np.pi/2
+
+        ecu_cmd = ECU(motor, servo_to_goal)
+        #ecu_cmd = ECU(throttle, steering)
         nh.publish(ecu_cmd)
         rate.sleep()
 
@@ -98,5 +119,6 @@ if __name__ == "__main__":
     try:
         main()
     except rospy.ROSInterruptException:
+        log_track_data(x_track, y_track)
         rospy.logfatal("ROS Interrupt. Shutting down speed_controller node")
         pass
